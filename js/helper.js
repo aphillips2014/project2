260ee07c44b5998a165cc712514dfed1d717b5e2
Andrew Phillips <phillips4energy@gmail.com>
2015-04-12 09:57:44 -0500

    Interactive Resume Updates
    Updates done while going through Javascript Basics class

--- js/helper.js
+++ js/helper.js
@@ -16,6 +16,7 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLcontact = '<li class="flex-item"><span class="orange-text">contact</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
---
+++ js/resumeBuilder.js
@@ -0,0 +1,75 @@
+var education = {   /* Education object */
+ 	"schools": [    /* Schools Array with 2 school objects  */
+ 		{
+ 			"name": "Texas A&M-Commerce",
+ 			"location": "Commerce, TX",
+ 			"degree": "BS",
+ 			"majors": ["Chemistry"],
+ 			"dates": 1996,
+ 			"url": "http://www.tamuc.edu"
+ 		}
+ 	],
+	"onlinecourses": [
+		{
+			"title" : "Front-End Web Developer",
+			"school": "Udacity",
+			"dates": 2015,
+			"url": "https://www.udacity.com/course/nd001"
+		}
+	]
+}
+
+var work = { 
+ 	"jobs": [
+ 		{
+ 			"employer": "Raytheon",
+ 			"title": "Associate Software Engineer",
+ 			"location": "Greenville, TX",
+ 			"dates": "1996-2000",
+ 			"description": "Design and develop defense systems for domestic and foreign governments. "
+ 		},
+ 		{
+ 			"employer": "AT&T",
+ 			"title": "Solution Engineer",
+ 			"location": "Richardson, TX",
+ 			"dates": "2000-Present",
+ 			"description": "Provide solutions for our monitoring environment."
+ 		}
+ 	]
+}
+
+var projects = { 
+ 	"project": [
+ 		{
+ 			"title": "Rivet Joint",
+ 			"dates": "1996-1998",
+ 			"description": "Developed operator work station for Rivet Joint aircraft",
+ 			"images": ["images/rivet1.jpg", "images/rivet2.jpg"]		
+ 		},
+ 		{
+ 			"title": "OLAM",
+ 			"dates": "2001-2003",
+ 			"description": "Developed web and backend code for online billing and minutes usage",
+ 			"images": ["images/att.jpg"]		
+ 		}
+ 	]
+}
+
+var bio = { 
+	"name" : "Andrew T. Phillips",
+	"role" : "Solution Engineer",
+ 	"contacts": {
+ 			"mobile": "972-822-6610",
+ 			"email": "phillips4energy@gmail.com",
+ 			"github": "https://github.com/aphillips2014",
+ 			"twitter": "@atphillips",
+ 			"location": "Dallas"		
+ 	 },
+ 	 "welcomeMessage": "Welcome to my resume page.",
+ 	 "skills": ["team player", "coder", "cook"
+ 	 ],
+ 	 "bioPic": "images/fry.jpg"
+}
+
+
+
